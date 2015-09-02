38a5120d5df586dc423ebec7775f544dc95a126e
will <chenium@gmail.com>
2015-09-02 14:53:46 -0400

    Pointer requires nullable

--- XLForm/XL/Descriptors/XLFormDescriptor.h
+++ XLForm/XL/Descriptors/XLFormDescriptor.h
@@ -58,7 +58,7 @@ typedef NS_OPTIONS(NSUInteger, XLFormRowNavigationOptions) {
 
 @property (weak, nullable) id<XLFormDescriptorDelegate> delegate;
 
--(nonnull instancetype)initWithTitle:(NSString *)title;
+-(nonnull instancetype)initWithTitle:(nullable NSString *)title;
 +(nonnull instancetype)formDescriptor;
 +(nonnull instancetype)formDescriptorWithTitle:(nullable NSString *)title;
 
